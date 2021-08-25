3a3a1097fcff6128be9b9b5260715f49c452b0c0
Hamza Soban <89018013+HamzaSoban0@users.noreply.github.com>
2021-08-25 12:28:42 +0500

    Add files via upload

---
+++ Responsive website/app.js
@@ -0,0 +1,9 @@
+const menuicon = document.querySelector(".menuicon");
+const menu = document.querySelector(".menu");
+
+
+menuicon.addEventListener('click', () => {
+
+	menu.classList.toggle("open");
+});
+
